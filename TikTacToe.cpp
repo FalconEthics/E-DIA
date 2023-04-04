70bf3f44e5de658fb9df25bd2ba0f8e4e360a024
Soumik Das <79033574+FalconEthics@users.noreply.github.com>
2023-04-04 21:04:17 +0530

    updated the spacing

--- TikTacToe.cpp
+++ TikTacToe.cpp
@@ -1,115 +1,160 @@
 #include <iostream>
 using namespace std;
-char matrix[3][3] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+const int BOARD_SIZE = 3;
+
+char board[BOARD_SIZE][BOARD_SIZE] = { {'1','2','3'},
+                                       {'4','5','6'},
+                                       {'7','8','9'} };
+
 char player = 'X';
-void Draw()
-{
-	system("cls");
-	cout << "Tic Tac Toe v1.0" << endl;
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			cout << matrix[i][j] << " ";
-		}
-		cout << endl;
-	}
+
+void drawBoard() {
+    system("cls");
+    cout << "\n\tTic Tac Toe\n\n";
+    cout << "Player 1 (X) - Player 2 (O)\n\n";
+    cout << "     |     |     \n";
+    cout << "  " << board[0][0] << "  |  " << board[0][1] << "  |  " << board[0][2] << endl;
+    cout << "_____|_____|_____\n";
+    cout << "     |     |     \n";
+    cout << "  " << board[1][0] << "  |  " << board[1][1] << "  |  " << board[1][2] << endl;
+    cout << "_____|_____|_____\n";
+    cout << "     |     |     \n";
+    cout << "  " << board[2][0] << "  |  " << board[2][1] << "  |  " << board[2][2] << endl;
+    cout << "     |     |     \n\n";
 }
-void Input()
-{
-	int a;
-	cout << "Press the number of the field: ";
-	cin >> a;
 
-	if (a == 1)
-		matrix[0][0] = player;
-	else if (a == 2)
-		matrix[0][1] = player;
-	else if (a == 3)
-		matrix[0][2] = player;
-	else if (a == 4)
-		matrix[1][0] = player;
-	else if (a == 5)
-		matrix[1][1] = player;
-	else if (a == 6)
-		matrix[1][2] = player;
-	else if (a == 7)
-		matrix[2][0] = player;
-	else if (a == 8)
-		matrix[2][1] = player;
-	else if (a == 9)
-		matrix[2][2] = player;
+void playerTurn() {
+    int choice;
+    cout << "Player " << player << ", enter a number: ";
+    cin >> choice;
+    switch (choice) {
+        case 1:
+            if (board[0][0] == '1')
+                board[0][0] = player;
+            else {
+                cout << "Invalid move!";
+                playerTurn();
+            }
+            break;
+        case 2:
+            if (board[0][1] == '2')
+                board[0][1] = player;
+            else {
+                cout << "Invalid move!";
+                playerTurn();
+            }
+            break;
+        case 3:
+            if (board[0][2] == '3')
+                board[0][2] = player;
+            else {
+                cout << "Invalid move!";
+                playerTurn();
+            }
+            break;
+        case 4:
+            if (board[1][0] == '4')
+                board[1][0] = player;
+            else {
+                cout << "Invalid move!";
+                playerTurn();
+            }
+            break;
+        case 5:
+            if (board[1][1] == '5')
+                board[1][1] = player;
+            else {
+                cout << "Invalid move!";
+                playerTurn();
+            }
+            break;
+        case 6:
+            if (board[1][2] == '6')
+                board[1][2] = player;
+            else {
+                cout << "Invalid move!";
+                playerTurn();
+            }
+            break;
+        case 7:
+            if (board[2][0] == '7')
+                board[2][0] = player;
+            else {
+                cout << "Invalid move!";
+                playerTurn();
+            }
+            break;
+        case 8:
+            if (board[2][1] == '8')
+                board[2][1] = player;
+            else {
+                cout << "Invalid move!";
+                playerTurn();
+            }
+            break;
+
+        case 9:
+            if (board[2][2] == '9')
+                board[2][2] = player;
+            else {
+                cout << "Invalid move!";
+                playerTurn();
+            }
+            break;
+        default:
+            cout << "Invalid move!";
+            playerTurn();
+            break;
+    }
 }
-void TogglePlayer()
-{
-	if (player == 'X')
-		player = 'O';
-	else
-		player = 'X';
+
+bool isGameOver() {
+// Check rows for a win
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        if (board[i][0] == board[i][1] && board[i][1] == board[i][2])
+            return true;
+    }
+// Check columns for a win
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        if (board[0][i] == board[1][i] && board[1][i] == board[2][i])
+            return true;
+    }
+// Check diagonals for a win
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2])
+        return true;
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0])
+        return true;
+// Check for a tie
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        for (int j = 0; j < BOARD_SIZE; j++) {
+            if (board[i][j] != 'X' && board[i][j] != 'O')
+                return false;
+        }
+    }
+    return true;
 }
-char Win()
-{
-	//first player
-	if (matrix[0][0] == 'X' && matrix[0][1] == 'X' && matrix[0][2] == 'X')
-		return 'X';
-	if (matrix[1][0] == 'X' && matrix[1][1] == 'X' && matrix[1][2] == 'X')
-		return 'X';
-	if (matrix[2][0] == 'X' && matrix[2][1] == 'X' && matrix[2][2] == 'X')
-		return 'X';
 
-	if (matrix[0][0] == 'X' && matrix[1][0] == 'X' && matrix[2][0] == 'X')
-		return 'X';
-	if (matrix[0][1] == 'X' && matrix[1][1] == 'X' && matrix[2][1] == 'X')
-		return 'X';
-	if (matrix[0][2] == 'X' && matrix[1][2] == 'X' && matrix[2][2] == 'X')
-		return 'X';
-
-	if (matrix[0][0] == 'X' && matrix[1][1] == 'X' && matrix[2][2] == 'X')
-		return 'X';
-	if (matrix[2][0] == 'X' && matrix[1][1] == 'X' && matrix[0][2] == 'X')
-		return 'X';
-
-	//second player
-	if (matrix[0][0] == 'O' && matrix[0][1] == 'O' && matrix[0][2] == 'O')
-		return 'O';
-	if (matrix[1][0] == 'O' && matrix[1][1] == 'O' && matrix[1][2] == 'O')
-		return 'O';
-	if (matrix[2][0] == 'O' && matrix[2][1] == 'O' && matrix[2][2] == 'O')
-		return 'O';
-
-	if (matrix[0][0] == 'O' && matrix[1][0] == 'O' && matrix[2][0] == 'O')
-		return 'O';
-	if (matrix[0][1] == 'O' && matrix[1][1] == 'O' && matrix[2][1] == 'O')
-		return 'O';
-	if (matrix[0][2] == 'O' && matrix[1][2] == 'O' && matrix[2][2] == 'O')
-		return 'O';
-
-	if (matrix[0][0] == 'O' && matrix[1][1] == 'O' && matrix[2][2] == 'O')
-		return 'O';
-	if (matrix[2][0] == 'O' && matrix[1][1] == 'O' && matrix[0][2] == 'O')
-		return 'O';
-
-	return '/';
+void switchPlayer() {
+    if (player == 'X')
+        player = 'O';
+    else
+        player = 'X';
 }
-int main()
-{
-	Draw();
-	while (1)
-	{
-		Input();
-		Draw();
-		if (Win() == 'X')
-		{
-			cout << "X wins!" << endl;
-			break;
-		}
-		else if (Win() == 'O')
-		{
-			cout << "O wins!" << endl;
-			break;
-		}
-		TogglePlayer();
-	}
-	system("pause");
-	return 0;
+
+int main() {
+    int choice;
+    bool gameover = false;
+    while (!gameover) {
+        drawBoard();
+        playerTurn();
+        gameover = isGameOver();
+        switchPlayer();
+    }
+    drawBoard();
+    if (player == 'X')
+        cout << "Player O wins!";
+    else
+        cout << "Player X wins!";
+    return 0;
 }
